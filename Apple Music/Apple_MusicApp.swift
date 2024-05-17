bb9a341e55cdaf51b3b6c00f72a7cfe8ba880000
Olga Mikhailova <foxx_fire@mail.ru>
2024-05-17 22:27:58 +0300

    Initial Commit

---
+++ Apple Music/Apple_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_MusicApp.swift
+//  Apple Music
+//
+//  Created by FoxxFire on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
